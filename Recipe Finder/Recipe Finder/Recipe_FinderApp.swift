59cdc8136db4ef5b8a4a2982e9f918467548c4c0
Nathan Geronimo <nathangero@yahoo.com>
2024-07-15 11:51:05 -0700

    refactor home screen; create view model

--- Recipe Finder/Recipe Finder/Recipe_FinderApp.swift
+++ Recipe Finder/Recipe Finder/Recipe_FinderApp.swift
@@ -9,12 +9,10 @@ import SwiftUI
 
 @main
 struct Recipe_FinderApp: App {
-    let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            HomeScreen()
         }
     }
 }
---
+++ Recipe Finder/Recipe Finder/Views/HomeScreen.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Recipe Finder
+//
+//  Created by Nathan Geronimo on 7/15/24.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    
+    @State private var viewModel = ViewModel()
+    
+    // MARK: - Body
+    
+    var body: some View {
+        Text("home Screen")
+    }
+    
+    
+    // MARK: - Views
+    
+    
+    // MARK: - Functions
+}
+
+
+#Preview {
+    HomeScreen()
+}
